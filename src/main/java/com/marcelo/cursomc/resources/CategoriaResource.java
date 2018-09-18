6e5f1da10aac975ba9642b61950153277cf529ca
Marcelo Silva Batista <silvabatistamarcelo@gmail.com>
2018-09-18 00:02:16 -0300

    REST Testadoy

--- src/main/java/com/marcelo/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/marcelo/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,31 @@
 package com.marcelo.cursomc.resources;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.marcelo.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST esta funcionando!";
+	public List<Categoria> listar() {
+		
+
+		Categoria cat1 = new Categoria(1, "Infomatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> categorias = new ArrayList<Categoria>();
+		
+		categorias.add(cat1);
+		categorias.add(cat2);
+		
+		return categorias;
 	}
 	
 }
