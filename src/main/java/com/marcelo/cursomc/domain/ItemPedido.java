c9293c26049e57e071c022734c6cd674dd918ddd
msbatista <silvabatistamarcelo@gmail.com>
2021-01-15 21:11:40 -0300

    Include total and subtotal

--- src/main/java/com/marcelo/cursomc/domain/ItemPedido.java
+++ src/main/java/com/marcelo/cursomc/domain/ItemPedido.java
@@ -56,6 +56,8 @@ public class ItemPedido implements Serializable {
 		this.preco = preco;
 	}
 
+	public Double getSubTotal() { return (preco - desconto) * quantidade; }
+
 	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
--- src/main/java/com/marcelo/cursomc/domain/Pedido.java
+++ src/main/java/com/marcelo/cursomc/domain/Pedido.java
@@ -100,6 +100,15 @@ public class Pedido implements Serializable {
 		return itens;
 	}
 
+	public double getValorTotal() {
+		double soma = 0;
+
+		for (ItemPedido ip : itens) {
+			soma += soma + ip.getSubTotal();
+		}
+		return soma;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
